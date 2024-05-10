43edc33f96e85785f6a710218c85cecd95a91b60
Sanket Arjun Pujari <104687744+Sanketpujari33@users.noreply.github.com>
2024-05-10 12:13:23 +0530

    Database Setup

--- todo-list-backend/src/config/db.js
+++ todo-list-backend/src/config/db.js
@@ -5,27 +5,11 @@ async function connectToDatabase() {
     const options = {
         useNewUrlParser: true,
         useUnifiedTopology: true,
-        useCreateIndex: true,
-        useFindAndModify: false,
-        socketTimeoutMS: 30000, // Increase the timeout as needed
-        connectTimeoutMS: 30000, // Increase the timeout as needed
     };
 
-    let retryCount = 0;
-    const maxRetries = 5;
-    const retryInterval = 5000; // 5 seconds
-
-    while (retryCount < maxRetries) {
-        try {
-            const conn = await mongoose.connect(connectionString, options);
-            console.log(`Connected to MongoDB! : ${conn.connection.host}`);
-            break; // Successful connection, exit loop
-        } catch (error) {
-            console.error(`Connection to MongoDB failed (attempt ${retryCount + 1}):`, error);
-            retryCount++;
-            await new Promise((resolve) => setTimeout(resolve, retryInterval));
-        }
-    }
+    mongoose.connect(connectionString, options)
+        .then(() => console.log('MongoDB connected'))
+        .catch(err => console.error('MongoDB connection error:', err));
 }
 
 // Call the function to connect to MongoDB
